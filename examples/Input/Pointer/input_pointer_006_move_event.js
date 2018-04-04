92ab7e165cd33a94ac3827323b3446d9dc52bb3e
fetaoily <fetaoily@gmail.com>
2018-04-04 11:11:32 +0800

    Input Pointer input_pointer_006_move_event.js

---
+++ examples/Input/Pointer/input_pointer_006_move_event.js
@@ -0,0 +1,39 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.load.spritesheet('balls', '/assets/sprites/balls.png', {frameWidth: 17, frameHeight: 17});
+    }
+
+    create () {
+      this.input.on('pointermove', (pointer) => {
+        this.add.image(pointer.x, pointer.y, 'balls', Phaser.Math.Between(0, 5));
+      }, this);
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+})();
