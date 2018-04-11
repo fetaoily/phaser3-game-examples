d9944cb8a8e1cdbe2b0132fdca8deba5f7c17f30
fetaoily <fetaoily@gmail.com>
2018-04-11 18:07:37 +0800

    Paths paths_003_circle_path.js

---
+++ examples/Paths/paths_003_circle_path.js
@@ -0,0 +1,61 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor () {
+      super();
+      this.follower = null;
+      this.path = null;
+      this.graphics = null;
+    }
+
+    preload () {
+    }
+
+    create () {
+      this.graphics = this.add.graphics();
+      //
+      this.follower = {t: 0, vec: new Phaser.Math.Vector2()};
+      this.path = new Phaser.Curves.Path();
+      this.path.add(new Phaser.Curves.Ellipse(400, 300, 100));
+      //
+      this.tweens.add({
+        targets: this.follower,
+        t: 1,
+        ease: 'Sine.easeInOut',
+        duration: 4000,
+        yoyo: true,
+        repeat: -1
+      });
+    }
+
+    update () {
+      //
+      this.graphics.clear();
+      this.graphics.lineStyle(2, 0xffffff, 1);
+      //
+      this.path.draw(this.graphics);
+      this.path.getPoint(this.follower.t, this.follower.vec);
+      //
+      this.graphics.fillStyle(0xff0000, 1);
+      this.graphics.fillCircle(this.follower.vec.x, this.follower.vec.y, 12);
+    }
+
+    render () {
+    }
+  }
+})();
