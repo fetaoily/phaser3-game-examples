5865b5962b135c6c49a84f6b8e00306f2aaf69cf
fetaoily <fetaoily@gmail.com>
2018-04-13 16:03:02 +0800

    Paths Curves paths_curves_025_spline_curve_from_array.js

---
+++ examples/Paths/Curves/paths_curves_025_spline_curve_from_array.js
@@ -0,0 +1,67 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor(config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor() {
+      super();
+      this.path = null;
+      this.curve = null;
+      this.points = null;
+      this.graphics = null;
+    }
+
+    preload() {}
+
+    create() {
+      this.graphics = this.add.graphics();
+      //
+      this.path = { t: 0, vec: new Phaser.Math.Vector2() };
+      //
+      this.points = [50, 300, 164, 246, 274, 342, 412, 257, 522, 341, 664, 264];
+      //
+      this.curve = new Phaser.Curves.Spline(this.points);
+      //
+      this.tweens.add({
+        targets: this.path,
+        t: 1,
+        ease: 'Sline.easeInOut',
+        duration: 2000,
+        yoyo: true,
+        repeat: -1
+      });
+    }
+
+    update() {
+      this.graphics.clear();
+      this.graphics.lineStyle(1, 0xffffff, 1);
+      //
+      this.curve.draw(this.graphics, 64);
+      this.graphics.fillStyle(0x00ff00, 1);
+      //
+      for (let i = 0; i < this.points.length; i++) {
+        this.graphics.fillCircle(this.points[i].x, this.points[i].y, 4);
+      }
+      //
+      this.curve.getPoint(this.path.t, this.path.vec);
+      //
+      this.graphics.fillStyle(0xff0000, 1);
+      this.graphics.fillCircle(this.path.vec.x, this.path.vec.y, 8);
+    }
+
+    render() {}
+  }
+})();
