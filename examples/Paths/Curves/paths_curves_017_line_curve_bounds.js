232d23b2fbf294a6d45afd8d15e24add0e275afd
fetaoily <fetaoily@gmail.com>
2018-04-13 13:07:26 +0800

    Paths Curves paths_curves_017_line_curve_bounds.js

---
+++ examples/Paths/Curves/paths_curves_017_line_curve_bounds.js
@@ -0,0 +1,121 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor(config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor() {
+      super();
+      this.path = null;
+      this.curve = null;
+      this.bounds = null;
+      this.graphics = null;
+    }
+
+    preload() {
+      this.load.spritesheet('dragcircle', 'assets/sprites/dragcircle.png', {
+        frameWidth: 16
+      });
+    }
+
+    create() {
+      this.graphics = this.add.graphics();
+      //
+      this.path = { t: 0, vec: new Phaser.Math.Vector2() };
+      //
+      this.bounds = new Phaser.Geom.Rectangle();
+      //
+      this.curve = new Phaser.Curves.Line([100, 100, 600, 400]);
+      //
+      this.curve = new Phaser.Curves.Line(
+        new Phaser.Math.Vector2(100, 100),
+        new Phaser.Math.Vector2(600, 400)
+      );
+      //
+      this.curve.getBounds(this.bounds);
+      //
+      let point0 = this.add
+        .image(this.curve.p0.x, this.curve.p0.y, 'dragcircle', 0)
+        .setInteractive();
+      let point1 = this.add
+        .image(this.curve.p1.x, this.curve.p1.y, 'dragcircle', 0)
+        .setInteractive();
+      //
+      // point0.data.set('vector', this.curve.p0);
+      // point1.data.set('vector', this.curve.p1);
+      point0.setData('vector', this.curve.p0);
+      point1.setData('vector', this.curve.p1);
+      //
+      this.input.setDraggable([point0, point1]);
+      //
+      // this.input.on('DRAG_START_EVENT', event => {
+      //   event.gameObject.setFrame(1);
+      // });
+      this.input.on('dragstart', (pointer, gameObject, dragX, dragY) => {
+        gameObject.setFrame(1);
+      });
+      //
+      // this.input.on('DRAG_EVENT',(event)=>{
+      //   event.gameObject.x = event.dragX;
+      //   event.gameObject.y = event.dragY;
+      //   //
+      //   event.gameObject.data.get('vector').set(event.dragX,event.dragY);
+      //   //
+      //   this.curve.getBounds(this.bounds);
+      // });
+      //
+      this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+        //
+        gameObject.data.get('vector').set(dragX, dragY);
+        this.curve.getBounds(this.bounds);
+      });
+      //
+      // this.input.on('DRAG_END_EVENT',(event)=>{
+      //   event.gameObject.setFrame(0);
+      // });
+      this.input.on('dragend', (pointer, gameObject) => {
+        gameObject.setFrame(0);
+      });
+      //
+      this.tweens.add({
+        targets: this.path,
+        t: 1,
+        ease: 'Sine.easeInOut',
+        duration: 2000,
+        yoyo: true,
+        repeat: -1
+      });
+    }
+
+    update() {
+      this.graphics.clear();
+      // Draw the bounds
+      this.graphics.lineStyle(1, 0x00ff00, 1).strokeRectShape(this.bounds);
+      // Draw the curve
+      this.graphics.lineStyle(2, 0xffffff, 1);
+      //
+      this.curve.draw(this.graphics);
+      //
+      this.curve.getPoint(this.path.t, this.path.vec);
+      //
+      this.graphics.fillStyle(0xffff00, 1);
+      this.graphics.fillRect(this.path.vec.x - 8, this.path.vec.y - 8, 16, 16);
+    }
+
+    render() {}
+  }
+})();
