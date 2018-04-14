3fc16ad419d8a143bd1463807dfeb37e67efbdf1
fetaoily <fetaoily@gmail.com>
2018-04-14 11:13:56 +0800

    Paths Followers paths_followers_003_mass_followers.js

---
+++ examples/Paths/Followers/paths_followers_003_mass_followers.js
@@ -0,0 +1,88 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+  class NewGame extends ErtaoGame {
+    constructor(config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor() {
+      super();
+    }
+    preload() {
+      this.load.image('ball', 'assets/particles/sparkle1.png');
+    }
+    create() {
+      let points = [
+        50,
+        300,
+        179,
+        449,
+        394,
+        498,
+        593,
+        455,
+        701,
+        338,
+        692,
+        190,
+        603,
+        76,
+        423,
+        41,
+        272,
+        78,
+        181,
+        186,
+        230,
+        328,
+        416,
+        395,
+        565,
+        327,
+        550,
+        202,
+        467,
+        149,
+        355,
+        164,
+        343,
+        254,
+        428,
+        303
+      ];
+      //
+      let curve = new Phaser.Curves.Spline(points);
+      //
+      for (let i = 0; i < 20; i++) {
+        let follower = this.add.follower(curve, 100, 100 + 30 * i, 'ball');
+        follower.setScale(0.5);
+        follower.setBlendMode(Phaser.BlendModes.ADD);
+        //
+        follower.startFollow({
+          // positionOnPath: true,
+          duration: 4500,
+          yoyo: true,
+          repeat: -1,
+          ease: 'Sine.easeInOut',
+          delay: i * 100
+        });
+      }
+      //
+      let graphics = this.add.graphics();
+      graphics.lineStyle(1, 0xffffff);
+      curve.draw(graphics);
+    }
+    update() {}
+    render() {}
+  }
+})();
