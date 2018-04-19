08a0b7bff376aa5308e8a2cc8721a5c2760f26e6
fetaoily <fetaoily@gmail.com>
2018-04-19 11:39:16 +0800

    Physics Impact physics_impact_007_collide_callback.js

---
+++ examples/Physics/Impact/physics_impact_007_collide_callback.js
@@ -0,0 +1,55 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      physics: {
+        default: 'impact',
+        impact: {debug: true}
+      },
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.load.image('block', 'assets/sprites/block.png');
+    }
+
+    create () {
+      let blockA = this.impact.add.image(60, 300, 'block');
+      let blockB = this.impact.add.image(730, 300, 'block');
+      //
+      blockA.setTypeA().setCheckAgainstB().setActive().setMaxVelocity(300);
+      blockB.setTypeB().setCheckAgainstA().setFixed();
+      //
+      blockA.setVelocityX(300);
+      // The callback will be sent the arguments: bodyA (which is the body of blockA in this case), the other body it hits and the axis
+      blockA.setCollideCallback(this.collide, this);
+    }
+
+    collide (bodyA, bodyB, axis) {
+      bodyB.gameObject.tint = 0xff0000;
+      bodyB.gameObject.setScale(2);
+      // bodyA.gameObject.setScale(3);
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+})();
