bf3f49dbba1232cac53d970302df5f34b6af5a6e
fetaoily <fetaoily@gmail.com>
2018-04-25 14:21:35 +0800

    Physics Metterjs physics_matterjs_025_multiple_physics_scenes.js

---
+++ examples/Physics/Matterjs/physics_matterjs_025_multiple_physics_scenes.js
@@ -0,0 +1,82 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      physics: {
+        default: 'matter',
+        matter: {
+          debug: true
+        }
+      },
+      scene: [SceneA, SceneB]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class SceneA extends ErtaoGameScene {
+    constructor () {
+      super({key: 'sceneA'});
+    }
+
+    preload () {
+      this.load.image('ball1', 'assets/sprites/pangball.png');
+    }
+
+    create () {
+      this.matter.world.setBounds(100, 100, 600, 400, 32, true, true, false, true);
+      //
+      for (let i = 0; i < 64; i++) {
+        let ball = this.matter.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(-600, 0), 'ball1');
+        ball.setCircle();
+        ball.setFriction(0.005);
+        ball.setBounce(1);
+      }
+      //
+      this.input.once('pointerdown', (event) => {
+        this.scene.start('sceneB')
+      }, this);
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+
+  class SceneB extends ErtaoGameScene {
+    constructor () {
+      super({key: 'sceneB'});
+    }
+
+    preload () {
+      this.load.image('ball2', 'assets/sprites/shinyball.png');
+    }
+
+    create () {
+      this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, false, true);
+      //
+      for (let i = 0; i < 64; i++) {
+        let ball = this.matter.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(-600, 0), 'ball2');
+        ball.setCircle();
+        ball.setFriction(0.005);
+        ball.setBounce(1);
+      }
+      //
+      this.input.once('pointerdown', () => {
+        this.scene.start('sceneA');
+      }, this)
+    }
+
+    update () {
+    }
+  }
+})();
