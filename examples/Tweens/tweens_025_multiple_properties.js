64e88941dc2e54cf87f7c3f0186f62912fd79ff0
fetaoily <fetaoily@gmail.com>
2018-04-26 17:06:49 +0800

    Tweens tweens_025_multiple_properties.js

---
+++ examples/Tweens/tweens_025_multiple_properties.js
@@ -0,0 +1,40 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.load.image('block', 'assets/sprites/block.png');
+    }
+
+    create () {
+      let image = this.add.image(100, 100, 'block');
+      //
+      this.tweens.add({
+        targets: image,
+        x: {value: 700, duration: 4000, ease: 'Power2'},
+        y: {value: 400, duration: 1500, ease: 'Bounce.easeOut'}
+      });
+    }
+
+    update () {
+    }
+  }
+})();
