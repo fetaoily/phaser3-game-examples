9d87015e573dd82ef54998474f5d35f79debf98a
fetaoily <fetaoily@gmail.com>
2018-05-30 08:35:05 +0800

    Tweens tweens_044_repeat_tween.js

---
+++ examples/Tweens/tweens_044_repeat_tween.js
@@ -0,0 +1,43 @@
+(() => {
+  'use strict';
+  let game;
+  let gameConfig;
+  window.onload = () => {
+    this.gameConfig = gameConfig = {
+      scene: [PlayGame]
+    };
+    this.game = game = new NewGame(this.gameConfig);
+  };
+
+  class NewGame extends ErtaoGame {
+    constructor (config) {
+      super(config);
+    }
+  }
+
+  class PlayGame extends ErtaoGameScene {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.load.image('block', 'assets/sprites/block.png');
+    }
+
+    create () {
+      let marker = this.add.image(100, 300, 'block').setAlpha(0.3);
+      let image = this.add.image(100, 300, 'block');
+      this.tweens.add({
+        targets: image,
+        x: 700,
+        duration: 1000,
+        ease: 'Power2',
+        repeat: -1,
+        delay: 1000
+      });
+    }
+
+    update () {
+    }
+  }
+})();
